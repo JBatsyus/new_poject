07455fa7c63338764b48500d2869b77a5c9b5df7
JBatsyus <ju.kalabina@gmail.com>
2024-05-06 09:49:09 +0200

    страница каталог- планшет, мобилка

--- build/js/index.js
+++ build/js/index.js
@@ -3,3 +3,30 @@ const header = document.querySelector('.header');
 const headerHeight = header.offsetHeight;
 document.documentElement.style.setProperty('--js-header-height', headerHeight + "px");
 //console.log("Высота хедера " + headerHeight + " пикселей");
+
+
+$(document).ready(function() {
+  // Слушатель события нажатия на документ
+  $(document).click(function(e) {
+    // Проверяем, не является ли целевой элемент дочерним элементом элемента .header-nav ul li
+    if (!$(e.target).closest('.header-nav ul li').length) {
+      // Если не является, то скрываем все подменю
+      $('.header-nav__submenu').css('transform', 'scaleY(0)');
+    }
+  });
+
+  // Слушатель события нажатия на элемент .header-nav ul li
+  $('.header-nav ul li').click(function() {
+    // Сворачиваем все подменю, кроме текущего
+    $('.header-nav__submenu').not($(this).find('.header-nav__submenu')).css('transform', 'scaleY(0)');
+
+    // Разворачиваем или сворачиваем текущее подменю
+    if ($(this).find('.header-nav__submenu').css('transform') == 'matrix(1, 0, 0, 1, 0, 0)') {
+      // Если подменю развернуто, то сворачиваем
+      $(this).find('.header-nav__submenu').css('transform', 'scaleY(0)');
+    } else {
+      // Если подменю свернуто, то разворачиваем
+      $(this).find('.header-nav__submenu').css('transform', 'scaleY(1)');
+    }
+  });
+});
